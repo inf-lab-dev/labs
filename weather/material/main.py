10c64e28279da13e19c14735ba9306372f979062
mariuslang <marius@mariuslang.de>
2025-01-03 23:35:06 +0100

    add given code

---
+++ weather/material/main.py
@@ -0,0 +1,34 @@
+from city_weather import CityWeather
+
+
+CITIES_LIST = [
+    "Bamberg",
+    "Paris",
+    "New York",
+    "Tokyo",
+    "Sydney",
+    "Cape Town",
+    "Rio de Janeiro",
+    "Mumbai",
+    "Dubai",
+    "Toronto",
+    "Shanghai",
+    "Mexico City",
+    "Bangkok",
+    "Moscow",
+    "Istanbul"
+]
+
+
+def fetch_weather(city_name):
+    # TODO: implement this function as explained in the instructions 
+    pass
+
+
+def extract_data(weather):
+    # TODO: implement this function as explained in the instructions 
+    pass
+
+
+if __name__ == "__main__":
+    # TODO: implement the main logic as explained in the instructions
